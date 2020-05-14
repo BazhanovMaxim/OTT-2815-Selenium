b88c2b8fd8307c92d7a895724f24f11597ab48fa
Maxim <bazhanov.maksim@list.ru>
2020-05-14 23:25:16 +0300

    Изменил импорты классов к записи видео для тестов

--- src/main/java/video/VideoWriter.java
+++ src/main/java/video/VideoWriter.java
@@ -1,6 +1,7 @@
 package video;
 
 import org.monte.media.Format;
+import org.monte.media.FormatKeys;
 import org.monte.media.math.Rational;
 import org.monte.screenrecorder.ScreenRecorder;
 
@@ -9,8 +10,17 @@ import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import static org.monte.media.FormatKeys.*;
-import static org.monte.media.VideoFormatKeys.*;
+import static org.monte.media.FormatKeys.MediaTypeKey;
+import static org.monte.media.FormatKeys.MimeTypeKey;
+import static org.monte.media.FormatKeys.MIME_AVI;
+import static org.monte.media.FormatKeys.EncodingKey;
+import static org.monte.media.VideoFormatKeys.CompressorNameKey;
+import static org.monte.media.VideoFormatKeys.ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE;
+import static org.monte.media.VideoFormatKeys.DepthKey;
+import static org.monte.media.FormatKeys.FrameRateKey;
+import static org.monte.media.VideoFormatKeys.QualityKey;
+import static org.monte.media.FormatKeys.KeyFrameIntervalKey;
+import static org.monte.media.FormatKeys.MediaType;
 
 public class VideoWriter {
 
@@ -25,8 +35,8 @@ public class VideoWriter {
         try {
             screenRecorder = new ScreenRecorder(gc,
                     gc.getBounds(),
-                    new Format(MediaTypeKey, MediaType.FILE, MimeTypeKey, MIME_AVI),
-                    new Format(MediaTypeKey, MediaType.VIDEO, EncodingKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
+                    new Format(MediaTypeKey, FormatKeys.MediaType.FILE, MimeTypeKey, MIME_AVI),
+                    new Format(MediaTypeKey, FormatKeys.MediaType.VIDEO, EncodingKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
                             CompressorNameKey, ENCODING_AVI_TECHSMITH_SCREEN_CAPTURE,
                             DepthKey, 24, FrameRateKey, Rational.valueOf(15),
                             QualityKey, 1.0f,
